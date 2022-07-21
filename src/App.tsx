7dcc49e801f9d9bf9b5264c655ee293927957b79
irina-lat <irochka.bars@mail.ru>
2022-07-21 13:33:57 +0300

    delete Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 import './App.css';
 import Count from "./Count";
 import Count2 from "./Count2";
-import Component1 from "./Component1";
-import Component2 from "./Component2";
 
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
         <div className={"app"}>
             <Count/>
             <Count2/>
-            <Component1/>
-            <Component2/>
         </div>
     );
 }
