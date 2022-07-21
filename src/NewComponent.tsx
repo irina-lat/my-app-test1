d94ce1dfd9f4e68d5f1435df0351550d821d8ce6
irina-lat <irochka.bars@mail.ru>
2022-07-21 11:08:36 +0300

    new

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 function NewComponent() {
     return (
-<<<<<<< HEAD
         <>
             <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut, autem consequuntur dolor
                 dolores
@@ -14,11 +13,13 @@ function NewComponent() {
                 ea earum et itaque libero maiores minima mollitia nemo nostrum odit omnis provident quos voluptas
                 voluptates!
             </div>
+
+            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut, autem consequuntur dolor
+                dolores
+                ea earum et itaque libero maiores minima mollitia nemo nostrum odit omnis provident quos voluptas
+                voluptates!
+            </div>
+            <div>5</div>
         </>
-=======
-        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut, autem consequuntur dolor dolores
-            ea earum et itaque libero maiores minima mollitia nemo nostrum odit omnis provident quos voluptas
-            voluptates!</div>
->>>>>>> origin/main
     )
 }
